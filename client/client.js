2ff862fd8216c473ad7edd0d8896b02a220acc5b
aromanen <aromanen@users.noreply.github.com>
2015-01-21 23:28:57 -0800

    fixed race between update and poll in client

--- client/client.js
+++ client/client.js
@@ -10,24 +10,31 @@ Template.light.rendered = function() {
 	checkbox.checkbox({
 		fireOnInit: false,
 		onChecked: function () {
-			Lights.update(that.data._id, {$set: {'state.on': true}});
+			that.blockUpdate = true;
+			Lights.update(that.data._id, {$set: {'state.on': true}}, null, function() {that.blockUpdate = false;});
 		},
 		onUnchecked: function () {
-			Lights.update(that.data._id, {$set: {'state.on': false}});
+			that.blockUpdate = true;
+			Lights.update(that.data._id, {$set: {'state.on': false}}, null, function() {that.blockUpdate = false;});
 		}
 	});
 
 	this.autorun(function() {
+		if (that.blockUpdate) {
+			return;
+		}
+
 		var data = Template.currentData();
 		if (data.state.reachable) {
 			checkbox.checkbox('enable');
-			if (data.state.on) {
-				checkbox.checkbox('check');
-			} else {
-				checkbox.checkbox('uncheck');
-			}
 		} else {
 			checkbox.checkbox('disable');
+		}
+
+		if (data.state.on) {
+			checkbox.checkbox('check');
+		} else {
+			checkbox.checkbox('uncheck');
 		}
 	})
 }
